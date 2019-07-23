3b243f1130f8471b30b4613cdc8e0454fa704cb7
Carlo Miras <mirascarlo934@gmail.com>
2019-07-23 22:03:49 +0800

    Initial commit
    This is used to copy the symphony library to the arduino library.
    It also updates the version in the version.h file.

--- esp8266/copySymphony.bat
+++ esp8266/copySymphony.bat
@@ -1,5 +1,10 @@
+for /f "tokens=3" %%v in (D:\git_symphony\esp8266\Symphony\version.h) do set version=%%v
+@echo version=%version%
+SET /A c = %version% + 1 
+echo #define SYMPHONY_VERSION %c% > D:\git_symphony\esp8266\Symphony\version.h
 copy D:\git_symphony\esp8266\Symphony\Symphony.* D:\projects\arduinolibraries\Symphony\*
 copy D:\git_symphony\esp8266\Symphony\FileManager.* D:\projects\arduinolibraries\Symphony\*
 copy D:\git_symphony\esp8266\Symphony\html.h D:\projects\arduinolibraries\Symphony\*
+copy D:\git_symphony\esp8266\Symphony\version.h D:\projects\arduinolibraries\Symphony\*
 copy D:\git_symphony\esp8266\Symphony\DeviceDiscovery.h D:\projects\arduinolibraries\Symphony\*
 copy D:\git_symphony\esp8266\Symphony\Product.* D:\projects\arduinolibraries\Symphony\*
