56fdbd6adc07420456b10343f23c4eba5f67f528
Celso Miras <celsmiras@gmail.com>
2020-03-10 21:24:26 +0800

    initial commit for docker files for study of springboot

---
+++ docker/springboot/runSB_dev.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm -v "$(pwd):/usr/src/myapp" --net test-network  -p 8080:8080 --name spring-boot1 spring-boot
---
+++ docker/springboot/runSB_instance2.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm --net test-network  -p 8081:8080 --name spring-boot2 spring-boot
