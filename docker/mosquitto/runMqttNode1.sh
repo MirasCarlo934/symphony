ccfcd6f9bf98f98295133f3b62bf3c37cd41683f
Celso Miras <celsmiras@gmail.com>
2020-04-10 21:53:10 +0800

    added run scripts for starting of 3 nodes for the testing of nginx mqtt load balancer

---
+++ docker/mosquitto/runMqttNode1.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm -it -p 18831:1883 -p 9001:9001 --net test-network --name mqtt1 eclipse-mosquitto
---
+++ docker/mosquitto/runMqttNode3.sh
@@ -0,0 +1,1 @@
+sudo docker run -it --rm -p 18833:1883 -p 9003:9001 --net test-network --name mqtt3 eclipse-mosquitto
