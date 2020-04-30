36b211e9e0a1097ba299c449b31b39cdfba0fa5a
Carlo Miras <mirascarlo934@gmail.com>
2020-04-30 18:21:09 +0800

    Fixed pom.xml in bm-core

--- BM-v5/bm-core/src/main/java/symphony/bm/core/BmCoreApplication.java
+++ BM-v5/bm-core/src/main/java/symphony/bm/core/BmCoreApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
