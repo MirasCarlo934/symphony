e83fa6877bc7fe2db96b800691193236a6f38b9b
Carlo Miras <mirascarlo934@gmail.com>
2020-05-14 11:02:23 +0800

    Created simple UI that consumes the get all things API

--- poc-ui/routes/index.js
+++ poc-ui/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var Request = require("request");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Request.get("http://ec2-3-135-233-237.us-east-2.compute.amazonaws.com:8080/things", (error, response, body) => {
+    if(error) {
+      return console.dir(error);
+    }
+    res.render('index', {
+      title: 'BeeMaestro',
+      things: body.things
+    });
+  });
 });
 
 module.exports = router;
